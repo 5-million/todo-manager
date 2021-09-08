d947ca120f0c7d37ebfda2612a2190822c12c1a3
jongseong-mac <kjs3870@naver.com>
2021-09-08 16:00:57 +0900

    property 파일 통합 적용

--- src/main/java/xyz/fivemillion/todomanager/TodoManagerApplication.java
+++ src/main/java/xyz/fivemillion/todomanager/TodoManagerApplication.java
@@ -2,22 +2,14 @@ package xyz.fivemillion.todomanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class TodoManagerApplication {
 
-    private static final String PROPERTIES = "spring.config.location=" +
-            "classpath:/application.yml" +
-            ",classpath:/todos.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(TodoManagerApplication.class)
-                .properties(PROPERTIES)
-                .run(args);
-//        SpringApplication.run(TodoManagerApplication.class, args);
+        SpringApplication.run(TodoManagerApplication.class, args);
     }
 
 }
